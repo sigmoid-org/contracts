392808ff523ee6cd4189104d816b2e8e51fed309
nitininhouse <nitin_g@me.iitr.ac.in>
2025-06-09 14:56:27 +0530

    writing main contract

--- story-protocol-project/src/mocks/SimpleNFT.sol
+++ story-protocol-project/src/mocks/SimpleNFT.sol
@@ -1,17 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.26;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SimpleNFT is ERC721, Ownable {
-    uint256 public nextTokenId;
-
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(msg.sender) {}
-
-    function mint(address to) public onlyOwner returns (uint256) {
-        uint256 tokenId = nextTokenId++;
-        _mint(to, tokenId);
-        return tokenId;
-    }
-}
